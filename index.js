db4b3f1a6b9e6fa34c36f2c2fae2f101e90d7dd5
ider tuguu <idertuguu0@gmail.com>
2021-03-09 19:12:21 +0800

    clc push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 9000;
 const path = require("path");
 const bodyParser = require("body-parser");
-
+const gridSource = [];
 //let urlencoded = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,8 +21,42 @@ app.post("/formData", (request, response) => {
 });
 app.listen(port, () => console.log("server running"));
 
+// function PerformSelect(par)
+//         {
+//             let sql = "SELECT Buleg.grp_code,Buleg.grp_name,Buleg.grp_define,Buleg.grp_comment, Desc.detail_value,Desc.detail_advice,Desc.detail_settings FROM Buleg INNER JOIN Desc ON Buleg.grp_code = Desc.grp_code where detail_value like '";
+//             string path = Application.StartupPath + "\\1.db;";
+//             string conString = "Data Source=" + path + "Version=3;";
+//             int r_too = 0;
+//             SQLiteConnection conn = new SQLiteConnection(conString);
+//             using (SQLiteCommand cmd = new SQLiteCommand())
+//             {
+//                 conn.Open();
+//                 cmd.Connection = conn;
+
+//                 SQLiteHelper sh = new SQLiteHelper(cmd);
+
+//                 try
+//                 {
+//                     sql = sql + par + "%';";
+//                     DataTable dt = sh.Select(sql);
+//                     grid2.DataSource = dt;
+//                     r_too = dt.Rows.Count;
+//                 }
+//                 catch (Exception ex)
+//                 {
+//                     ex.ToString();
+//                     DataTable dt = new DataTable();
+//                     dt.Columns.Add("Error");
+//                     dt.Rows.Add(ex.ToString());
+//                     grid2.DataSource = dt;
+//                 }
+//                 conn.Close();
+//             }
+//             return r_too;
+//         }
+//----------------------------------
+
 //year month day 3 hoorond nemeh
-var array = [];
 var ss0 = [];
 function ButtonClick(YearD, MonthD, DayD) {
   var Y = YearD;
@@ -42,14 +76,14 @@ function ButtonClick(YearD, MonthD, DayD) {
     tmp = tmp + D;
   }
   // огноог эхнээс нь цифрүүдийн нийлбэр нь 1 оронтой болтол нь цифрүүдээр задлан бичих
-  ss0 = _clc(Convert.ToInt32(tmp), CurrentPos);
+  var ss0 = _clc(tmp, CurrentPos);
   var tt0;
-  if (dt1.Value < Convert.ToDateTime("01/01/2000")) {
+  if (Y < parseInt("2000/01/01")) {
     //2000.01.01 өдрөөс өмнө төрсөн хүмүүсийн тухайд:
     tt0 = ss0[2];
     // эхний нийлбэрээс (өдөр х 2)-ийг хасах
-    if (dt1.Value.Day < 10) {
-      tt0 = tt0 - dt1.Value.Day * 2;
+    if (D < 10) {
+      tt0 = tt0 - D * 2;
     } else {
       tt0 =
         tt0 - Convert.ToInt16(gridSource.Rows[1].Cells[6].Value.ToString()) * 2;
@@ -70,70 +104,63 @@ function ButtonClick(YearD, MonthD, DayD) {
     gridSource.Rows[1].Cells[CurrentPos].Value = tt0;
     gridSource.Rows[1].Cells[CurrentPos].Style.BackColor = Color.LightSalmon;
   }
-  // гарсан тоог дахин цифрүүдийн нийлбэр нь 1 оронтой болтол нь цифрүүдээр задлан бичих
-  CurrentPos = CurrentPos + 1;
-  var ss1 = _clc(tt0, CurrentPos);
-  l = ss1[1] + 1;
+  // // гарсан тоог дахин цифрүүдийн нийлбэр нь 1 оронтой болтол нь цифрүүдээр задлан бичих
+  // CurrentPos = CurrentPos + 1;
+  // var ss1 = _clc(tt0, CurrentPos);
+  // l = ss1[1] + 1;
 }
 //
 function _calc(n) {
   var sum = 0;
-  while (n != 0) {
-    sum += n % 10;
-    n /= 10;
+  for (i = 0; i < n.length; i++) {
+    sum += parseInt(n[i]);
   }
   return sum;
 }
 
-// var ArrayList = [];
-// function  _clc(too, col)
-//         {
-//             var current_too = too;
-//             var current_col = col;
-//             var sum_too = 0;
-//             var ret_arr = [ ];
-//             for ( i = 0; i < too.ToString().Length; i++)
-//             {
-//                 sum_too = sum_too + Convert.ToInt32(too.ToString().Substring(i, 1));
-//             }
-//             var ret_too = 0;
-//             ArrayList  = new ArrayList();
-//             list.Add(current_too);
-//             list.Add(current_col);
-//             list.Add(sum_too);
-//              ret_arr = (int[])list.ToArray(typeof(int));
-//             var ret_arr =
-//             var _cycle = 1;
-//             while (_cycle == 1) // нэг оронтой тоо гартал үргэлжлүүлэх юм
-//             {
-//                 if (current_too.ToString().Length == 1)
-//                 {
-//                     _cycle = 0;
-//                 }
-//                 // "ret" тооны цифрүүдийг нэг бүрчлэн хойш нь цувуулан бичих
-//                 if (current_too.ToString().Length > 1)
-//                 {
-//                     for (int i = 0; i < current_too.ToString().Length; i++)
-//                     {
-//                         gridSource.Rows[1].Cells[current_col].Value = current_too.ToString().Substring(i, 1);
-//                         current_col = current_col + 1;
-//                     }
-//                     //цифрүүдийн нийлбэрийг олоод "ret" хувьсагч-д хадгалах
-//                     ret_too = _calc(current_too);
-//                     if (ret_too == 10) ret_too = 1;
-//                     // бодсон нийлбэр дүнгээ 2 дахь параметрт өгөгдсөн "col" багана дээр бичээд
-//                     // нийлбэр бичсэн нүдээ тусгай өнгөөр будах
-//                     gridSource.Rows[1].Cells[current_col].Value = ret_too;
-//                     if (ret_too.ToString().Length > 1 || current_too == 11 || current_too == 22 || current_too == 33)
-//                     {
-//                         gridSource.Rows[1].Cells[current_col].Style.BackColor = Color.LightSalmon;
-//                     }
-//                     current_col = current_col + 1;
-//                 }
-//                 current_too = ret_too;
-//             }
-//             ret_arr[0] = current_too;
-//             ret_arr[1] = current_col;
-//             ret_arr[2] = sum_too;
-//             return ret_arr;
-//         }
+function _clc(too, col) {
+  var current_too = too;
+  var current_col = col;
+  var sum_too = 0;
+  var ret_arr = [];
+  for (i = 0; i < too.length; i++) {
+    sum_too = sum_too + parseInt(too.substr(i, 1));
+  }
+  var ret_too = 0;
+  var listNum = [];
+  var gridSource = [];
+  var list = [];
+  list.push(current_too);
+  list.push(current_col);
+  list.push(sum_too);
+  //var ret_arr = parseInt(list);
+  var ret_arr = [];
+  var _cycle = 1;
+  console.log(current_too);
+  while (_cycle == 1) {
+    // нэг оронтой тоо гартал үргэлжлүүлэх юм
+    if (current_too.length == 1) {
+      console.log("1 tei tentsuu:");
+      _cycle = 0;
+    }
+    // "ret" тооны цифрүүдийг нэг бүрчлэн хойш нь цувуулан бичих
+    if (current_too.length > 1) {
+      for (i = 0; i < current_too.length; i++) {
+        listNum.push(parseInt(current_too.substr(i, 1)));
+        current_col = current_col + 1;
+      }
+      //цифрүүдийн нийлбэрийг олоод "ret" хувьсагч-д хадгалах
+      ret_too = _calc(current_too);
+      if (ret_too == 10) ret_too = 1;
+      // бодсон нийлбэр дүнгээ 2 дахь параметрт өгөгдсөн "col" багана дээр бичээд
+      // нийлбэр бичсэн нүдээ тусгай өнгөөр будах
+      gridSource.push(ret_too);
+      current_col = current_col + 1;
+    }
+    current_too = ret_too.toString();
+  }
+  ret_arr.push(current_too);
+  ret_arr.push(current_col);
+  ret_arr.push(sum_too);
+  return ret_arr;
+}
--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,4 @@
 let form = document.querySelector("form");
-
 form.onsubmit = sendData;
 function sendData(e) {
   e.preventDefault();
@@ -21,40 +20,31 @@ function sendData(e) {
       response.json();
     })
     .then((data) => {
-      console.log("ddd" + data);
+      console.log("data++");
+      console.log(data);
     })
     .catch((err) => console.log(err));
 }
-//db connect
-let db = new sqlite3.Database("./db/1.db", sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log("Connected to the chinook database.");
-});
+// //db connect
+// let db = new sqlite3.Database("./db/1.db", sqlite3.OPEN_READWRITE, (err) => {
+//   if (err) {
+//     console.error(err.message);
+//   }
+//   console.log("Connected to the chinook database.");
+// });
 //////////////////////////////////////////////////
 
 // functions
-function PerformSelect(par) {
-  var sql =
-    "SELECT Buleg.grp_code,Buleg.grp_name,Buleg.grp_define,Buleg.grp_comment, Desc.detail_value,Desc.detail_advice,Desc.detail_settings FROM Buleg INNER JOIN Desc ON Buleg.grp_code = Desc.grp_code where detail_value like '";
-  let db = new sqlite3.Database("./db/1.db", sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connected to the chinook database.");
-  });
-  return r_too;
+// function PerformSelect(par) {
+//   var sql =
+//     "SELECT Buleg.grp_code,Buleg.grp_name,Buleg.grp_define,Buleg.grp_comment, Desc.detail_value,Desc.detail_advice,Desc.detail_settings FROM Buleg INNER JOIN Desc ON Buleg.grp_code = Desc.grp_code where detail_value like '";
+//   let db = new sqlite3.Database("./db/1.db", sqlite3.OPEN_READWRITE, (err) => {
+//     if (err) {
+//       console.error(err.message);
+//     }
+//     console.log("Connected to the chinook database.");
+//   });
+//   return r_too;
 
-  return new Function("return x;"); // this |x| refers global |x|
-}
-
-//_calc function
-function _calc(n) {
-  var sum = 0;
-  while (n != 0) {
-    sum += n % 10;
-    n /= 10;
-  }
-  return sum;
-}
+//   return new Function("return x;"); // this |x| refers global |x|
+// }
