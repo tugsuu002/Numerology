6484f7b11689e5682bc670e44015961b5207ab5e
ider tuguu <idertuguu0@gmail.com>
2021-03-16 18:33:32 +0800

    10.7 16.7 deer aldaa zaasan

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const express = require("express");
 const app = express();
 const port = 9000;
 const path = require("path");
-
 import bodyParser from "body-parser";
-// const { Console } = require("console");
-// const sqlite3 = require("sqlite3").verbose();
 import { ButtonClick } from "./controller";
-//let urlencoded = bodyParser.urlencoded({ extended: false });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,7 +15,6 @@ app.get("/", (request, response) => {
   // const result = calculate(year, month, day);
   // const arr = func(result);
   // response.send({ result: arr });
-  response.sendFile(path.join(__dirname + "/index.html"));
 });
 app.post("/formData", (request, response) => {
   //ButtonClick function year, month, day 3 iig damjuulsan //
